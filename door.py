c87f838234051c247fe9bf2469709874364e5c71
NandaNxD <nanven9@gmail.com>
2021-11-15 01:04:31 +0530

    First Commit

---
+++ door.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+import calendar
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+TRIG=4
+ECHO=27
+print('Distance measurement in Progress')
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG,False)
+print('Waiting for sensor to settle')
+time.sleep(2)
+GPIO.output(TRIG,True)
+time.sleep(0.00001)
+GPIO.output(TRIG,False)
+print('hello')
+
+while GPIO.input(ECHO)==0:
+    pulse_start=time.time()
+    
+while GPIO.input(ECHO)==1:
+    pulse_end=time.time()
+    
+pulse_duration=pulse_end-pulse_start
+distance=pulse_duration*17150
+distance=round(distance,2)
+print(distance)
+
+cred = credentials.Certificate('doorsense-f9327d1a0fb5.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
